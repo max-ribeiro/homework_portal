789b72b7b8a07644f486ce90f70df48164d20d4f
max-ribeiro <max_ribeiro@live.com>
2025-08-23 06:15:10 -0300

    feat: criação da logica inicial dos subjects

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
+    private Subject $subject;
+    public function __construct(Subject $subject)
+    {
+        $this->subject = $subject;
+    }
     //
+    public function index() {
+        return $this->subject->all();
+    }
+
+    public function store(Request $request) {
+        return $this->subject->create($request->all());
+    }
+
+    public function show(int $id) {
+        return $this->subject->find($id);
+    }
+
+    public function update(Request $request, int $id) {
+        $subject = $this->subject->find($id);
+        if(!$subject) {
+            return response()->json([
+                'message' => 'subject not found'
+            ], 404);
+        }
+        $subject->fill($request->all());
+        $subject->save();
+        return $subject;
+    }
+    public function destroy(int $id) {
+        $subject = $this->subject->find($id);
+        if(!$subject) {
+            return response()->json([
+                'message' => 'subject not found'
+            ], 404);
+        }
+        $status = $subject->delete($id);
+        return response()->json([
+            'message'=> 'subject deleted'
+        ], 200);
+    }
 }
---
+++ docker/mysql-init/init.sql
@@ -0,0 +1,4 @@
+GRANT ALL PRIVILEGES ON homework_portal.* TO 'user'@'%';
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON homework_portal_tests.* TO 'user'@'%';
+FLUSH PRIVILEGES;
