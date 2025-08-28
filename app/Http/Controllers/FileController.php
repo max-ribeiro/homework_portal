884ee959dc75d908db5c870ac299ec94197f6c48
max-ribeiro <max_ribeiro@live.com>
2025-08-28 19:09:44 -0300

    feat: controller files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\File;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
+    private File $file;
+    public function __construct(File $file)
+    {
+        $this->file = $file;
+    }
     //
+    public function index() {
+        return $this->file->all();
+    }
+
+    public function store(Request $request) {
+        return $this->file->create($request->all());
+    }
+
+    public function show(int $id) {
+        return $this->file->find($id);
+    }
+
+    public function update(Request $request, int $id) {
+        $file = $this->file->find($id);
+        if(!$file) {
+            return response()->json([
+                'message' => 'file not found'
+            ], 404);
+        }
+        $file->fill($request->all());
+        $file->save();
+        return $file;
+    }
+    public function destroy(int $id) {
+        $file = $this->file->find($id);
+        if(!$file) {
+            return response()->json([
+                'message' => 'file not found'
+            ], 404);
+        }
+        $status = $file->delete($id);
+        return response()->json([
+            'message'=> 'file deleted'
+        ], 200);
+    }
 }
--- app/Models/File.php
+++ app/Models/File.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class File extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'content',
+        'file_type',
+    ];
 }
